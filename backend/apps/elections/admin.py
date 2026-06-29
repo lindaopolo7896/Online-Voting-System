464b315e819ce63b1e7f9249eb5d9e41dd372d79
Varriable <ramadhanabdilatif13@gmail.com>
2026-06-29 21:22:37 +0300

    Admin panel added

--- backend/apps/elections/admin.py
+++ backend/apps/elections/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Position, Participant, Candidate
 
-# Register your models here.
+class PositionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'organisation', 'election', 'status')
+    search_fields = ('name', 'organisation__name', 'election__name')
+    list_filter = ('status','organisation', 'election')
+
+class ParticipantAdmin(admin.ModelAdmin):
+    list_display = ('membership', 'election', 'has_voted', 'token', 'check_in_tx_hash', 'created_at')
+    search_fields = ('membership__user__email', 'election__name', 'token__token')
+    list_filter = ('has_voted', 'election')
+
+class CandidateAdmin(admin.ModelAdmin):
+    list_display = ('membership', 'election', 'position', 'status', 'created_at')
+    search_fields = ('membership__user__email', 'election__name', 'position__name')
+    list_filter = ('status', 'election', 'position')
+    
--- backend/apps/users/admin.py
+++ backend/apps/users/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import Organisation, User, Membership, Election, PermissionRecord, Log
 
-# Register your models here.
+class OrganisationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'created_at')
+    search_fields = ('name',)
+    list_filter = ('created_at',)
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'is_verified', 'is_staff', 'is_active')
+    search_fields = ('email', 'first_name', 'last_name')
+    list_filter = ('is_verified', 'is_staff', 'is_active')
+
+class MembershipAdmin(admin.ModelAdmin):
+    list_display = ('organisation', 'user', 'role', 'currently_active', 'is_active', 'created_at')
+    search_fields = ('organisation__name', 'user__email', 'role')
+    list_filter = ('role', 'currently_active', 'is_active')
+
+class ElectionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'date_time_occuring', 'date_time_ending')
+    search_fields = ('name', 'description')
+    list_filter = ('date_time_occuring', 'date_time_ending')
+
+class PermissionRecordAdmin(admin.ModelAdmin):
+    list_display = ('membership', 'codename', 'election', 'assigned_at')
+    search_fields = ('membership__user__email', 'codename', 'election__name')
+    list_filter = ('codename', 'election')
+
+class LogAdmin(admin.ModelAdmin):
+    list_display = ('membership', 'election', 'codename', 'created_at')
+    search_fields = ('membership__user__email', 'election__name', 'codename')
+    list_filter = ('codename', 'election')
